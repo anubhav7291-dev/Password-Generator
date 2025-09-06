80a004b4c6642aecf15024fd00f9b4894964b6bb
anubhav7291-dev <anubhavgupta1511@gmail.com>
2025-09-06 14:23:09 +0000

    .. ....

--- passwordGenerator/vite.config.js
+++ passwordGenerator/vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/Password-Generator/",
+  build: {
+    outDir: "docs", // 👈 dist ke jagah docs me build hoga
+  },
   plugins: [
     react(),
     tailwindcss()
   ]
 })
+
 
